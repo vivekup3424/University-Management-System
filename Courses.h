f8da1fb1eafdda85ebdcd7dbe6a5f5e9be37acb2
vivekup3424 <vivek.hygfrt324@gmail.com>
2022-10-24 13:47:15 +0530

    I have made some more changes to Courses.h and have set up Student.h for now

--- Courses.h
+++ Courses.h
@@ -5,9 +5,9 @@ using namespace std;
 class Courses //this class maintains record for the courses a student is taking,
 {			  //and his attadannce,marks and other records of each course.
 private:
-	vector<string> courses_enrolled; //iD of all the courses taken by a student
-	vector<float> course_wise_attandance_percentage;
-	vector<float> course_wise_marks;
+	vector<string> courses_enrolled; //name of all the courses taken by a student
+	vector<float> course_wise_attandance_percentage;//coursewise attendance of students
+	vector<int> course_wise_marks;// coursewise marks of students in a given semester
 	int total_no_courses;
 public:
 	Courses()=default;
@@ -15,13 +15,13 @@ public:
 	//setters
 	void set_courses_enrolled(string);
 	void set_coursewise_attendance_percentage(float);
-	void set_course_wise_marks(float);
-	void change_course_enrolled(int ,string ,float ,int);
+	void set_course_wise_marks(int); //I have changed this from float to int
+	void change_course_enrolled(int ,string ,float ,int); //changing the details of nth course
 	//getters
-	string get_courses_enrolled(int);
-	float get_coursewise_attendance_percentage(int);
+	string get_courses_enrolled(int);//getting the name of nth course info
+	float get_coursewise_attendance_percentage(int);//getting the attendance of nth course
 	float get_course_wise_marks(int);
-    int get_total_no_courses();
+    int get_total_no_courses();//get total number of courses
 };
 
 
@@ -29,7 +29,7 @@ public:
 
 void Courses::set_courses_enrolled(string ce)
 {
-	this->courses_enrolled.push_back(ce);
+	this->courses_enrolled.push_back(ce);//appending the "ce" course in course-list
 	this->total_no_courses = courses_enrolled.size();
 }
 void Courses::set_coursewise_attendance_percentage(float cap) 
@@ -40,7 +40,7 @@ void Courses::set_coursewise_attendance_percentage(float cap)
 	}
 	this->course_wise_attandance_percentage.push_back(cap);
 }
-void Courses::set_course_wise_marks(float cm)
+void Courses::set_course_wise_marks(int cm)//i have changed the parameters data-type from float to int
 {
 	for (int i = 0; i < courses_enrolled.size(); i++)
 	{
@@ -58,11 +58,13 @@ void Courses::change_course_enrolled(int index,string course,float att,int marks
 	this->course_wise_attandance_percentage.at(index) = att;
 	this->course_wise_marks.at(index) = marks;
 }
+
 //returns course name at a particular index
 string Courses::get_courses_enrolled(int index) 
 {
 	return courses_enrolled[index];
 }
+
 float Courses::get_coursewise_attendance_percentage(int index)
 {
 	return course_wise_attandance_percentage[index];
---
+++ Student.h
@@ -0,0 +1,68 @@
+
+#include"Courses.h" //course is aggregating to a student
+#include"AcademicRecord.h"
+#include "Person.h"
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+//We are using composition for courses because two students can have a 
+//set of course record, hence I am not using inheritance between Student and
+//Courses
+class Student:public AcademicRecord,public Person
+{
+private:
+	Courses A;//A contains all information about the courses a student has opt for.
+	//int index; //I might remove this one
+	string institute_email;
+
+public:
+	Student();
+	~Student();
+	//Using polymorphism between methods
+	void ShowStudentAcademicRecord(); //shows the academic record of current student
+	void ShowStudentCoursesInfo(); //getting the information for all courses of student
+	
+	//setter
+	void set_institute_email(string email);
+
+	//getter
+	string get_institute_email();
+
+};
+
+void Student::ShowStudentAcademicRecord()
+{
+	cout<<"\nThe Programme name of student  : "<<this->get_program_name()<<"\n";
+	cout<<"\nThe Admission number of student: "<<this->get_admission_no()<<"\n";
+	cout<<"\nThe Enroll number of student   : "<<this->get_enroll_no()<<"\n";
+	cout<<"\nThe Session of student: ( "<<this->get_session().first<<","<<this->get_session().second<<" ).\n";
+	cout<<"\nThe CGPA of student: "<<this->get_CGPA()<<"\n";
+	cout<<"\nThe total credits of student: "<<this->get_total_credits()<<"\n";
+}
+void Student::ShowStudentCoursesInfo()
+{
+
+	int size = this->A.get_total_no_courses();
+	cout<<"Attendance Percentage";
+	cout<<"Course Name    Attendance Percentage    Marks";
+	for (int i = 0; i < size; i++)
+	{
+		cout<<this->A.get_courses_enrolled(i)<<"\t";
+		cout<<this->A.get_coursewise_attendance_percentage(i)<<"\t";
+		cout<<this->A.get_course_wise_marks(i)<<"\t";
+		cout<<"\n";
+	}	
+	cout << "\nThe total number of courses are " << this->A.get_total_no_courses()<<endl;
+}
+
+void Student::set_institute_email(string email)
+{
+	this->institute_email = email;
+}
+
+string Student::get_institute_email()
+{
+	return this->institute_email;
+}
